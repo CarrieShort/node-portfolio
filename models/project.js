9ae58fd2c27543cf72d8591a1b622c127df78853
CarrieShort <carrie.r.phillips@gmail.com>
2017-11-14 15:14:02 -0800

    create schema for projects

---
+++ models/project.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+var projectSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  imagePath: { type: String, required: true },
+  shortDesc: { type: String, required: true },
+  overViewHTML: { type: String, required: true },
+  githubUrl: { type: String },
+  liveProject: { type: String },
+  designImages: [String],
+  designHTML: { type: String }
+});
+
+module.exports = mongoose.model('Project', projectSchema);
