3a712728ff9658161db01cc4af6b8d6cddce591c
CarrieShort <carrie.r.phillips@gmail.com>
2017-11-17 13:22:19 -0800

    add route for update

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -37,6 +37,19 @@ projectRouter.get('/projects/featured', (req, res) => {
   });
 });
 
+projectRouter.put('/projects/:id', bodyParser, (req, res) => {
+  var projectData = req.body;
+  delete projectData._id;
+  Project.update({ _id: req.params.id }, projectData, (err, data) => {
+    if (err) {
+        return res.status(500).json({
+      msg: 'there was an error while trying to retrieve data'
+      });
+    }
+    res.status(200).json(data);
+  });
+});
+
 projectRouter.delete('/projects/:id', (req, res) => {
   Project.remove({ _id: req.params.id }, (err) => {
     if (err) {
@@ -45,7 +58,7 @@ projectRouter.delete('/projects/:id', (req, res) => {
       });
     }
     res.status(200).json({
-      msg: 'project has been removed'
+      msg: req.params.id + ' project has been removed'
     });
   });
 });
