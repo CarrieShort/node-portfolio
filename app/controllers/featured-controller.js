c9e08f422eea8877f55241f0a98149de4bec32d0
CarrieShort <carrie.r.phillips@gmail.com>
2018-01-22 19:34:33 -0800

    html and css changes to project detail view

--- app/controllers/featured-controller.js
+++ app/controllers/featured-controller.js
@@ -1,11 +1,8 @@
 module.exports = function(app) {
   app.controller('FeaturedController', ['$http', function($http) {
-    console.log('controller activated');
     this.go = function() {
-      console.log('go');
     };
     this.getFeatured = function() {
-      console.log('getFeatured runs');
       $http({
         method: 'GET',
         url: 'api/projects/featured'
