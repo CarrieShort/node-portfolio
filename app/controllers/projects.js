4501f80a72aef65502baa6a1d246e10e8bfb9f06
CarrieShort <carrie.r.phillips@gmail.com>
2018-02-22 15:41:40 -0800

    add filter on load option to the mixitup directive, add links to home page banner

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -15,6 +15,8 @@ module.exports = function(app) {
       });
     };
     this.getAll = function() {
+      var filter = $location.hash();
+      this.filter = filter;
       $http({
         method: 'GET',
         url: 'api/projects'
--- app/directives/mixitup.js
+++ app/directives/mixitup.js
@@ -7,10 +7,16 @@ module.exports = function(app) {
      scope: {
        entities: '='
      },
-     link: function(scope, element, attr) {
+     link: function(scope, element, attrs, controller) {
                scope.$on('projectsLoaded', function(){
                  var mixer = mixitup(element);
-                 console.log('i heard a broadcast');
+                 console.log(scope.$parent.projectCtrl.filter)
+                 if (scope.$parent.projectCtrl.filter) {
+                   setTimeout(function() { mixer.filter('.category-' + scope.$parent.projectCtrl.filter); }, 10);
+                 }
+                 $('.filter ul li').click(function() {
+                   history.pushState(null, null, '/portfolio#' + $(this).attr('data-filter').split('.category-').pop());
+                 });
                });
      }
    }
